87dc5af84b6cccddb299af33f8771660028489e5
Tal Mitzmacher <taltul432@gmail.com>
2025-09-25 16:38:06 +0300

    Update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:  [    "./src/**/*.{js,jsx,ts,tsx}",  ],
+  content: [
+    "./index.html",                 // חשוב ל-Vite
+    "./src/**/*.{js,jsx,ts,tsx}",   // כל הקבצים בקוד
+  ],
+  safelist: [
+    // כאן מכניסים מחלקות שיכולות להופיע בצורה דינמית
+    "bg-red-500",
+    "bg-blue-500",
+    "bg-green-500",
+    "text-center",
+    "text-right",
+    "text-left",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
